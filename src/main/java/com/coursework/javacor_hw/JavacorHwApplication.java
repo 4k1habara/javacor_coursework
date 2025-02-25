d7f86f5b8771b47a16a97314ba27a227cd440b1d
shawt <shawtyhead@mail.ru>
2025-02-25 21:58:58 +0300

    init commit

---
+++ src/main/java/com/coursework/javacor_hw/JavacorHwApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.javacor_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacorHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacorHwApplication.class, args);
+	}
+
+}
