51f158a7388678396b2d251fefadcca6ec396c2d
shawt <shawtyhead@mail.ru>
2025-02-25 22:04:40 +0300

    examiner service is added

---
+++ src/main/java/com/coursework/javacor_hw/service/ExaminerServiceImpl.java
@@ -0,0 +1,28 @@
+package com.coursework.javacor_hw.service;
+
+import com.coursework.javacor_hw.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+@Service
+public class ExaminerServiceImpl implements ExaminerService {
+    private final JavaQuestionService javaQuestionService;
+
+    public ExaminerServiceImpl(JavaQuestionService javaQuestionService) {
+        this.javaQuestionService = javaQuestionService;
+    }
+
+    private Set<Question> questions = new HashSet<Question>();
+
+    @Override
+    public Set<Question> getQuestions(int amount) {
+        if (javaQuestionService.getAll().size() < amount) {
+            throw new NotEnoughQuestionsException();
+        }
+        while (questions.size() < amount) {
+            questions.add(javaQuestionService.getRandomQuestion());
+        }
+        return questions;
+    }
+}
